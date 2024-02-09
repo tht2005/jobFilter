adc9f2ea05d6eaf78d2f2937351a0480cbe8dde7
tht2005 <75089048+tht2005@users.noreply.github.com>
2024-02-09 15:11:45 +0700

    Update logo

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
 app.listen(3000);
 app.set('view engine', 'ejs');
 
+var svgFile = [];
+fs.readdirSync('./images').forEach(fileName => {
+    if(fileName.split('.')[1] === 'svg') {
+        svgFile[`./images/${fileName}`] = fs.readFileSync(`./images/${fileName}`).toString();
+    }
+});
+
 app.get('/', (req, res) => {
-    res.render('index', { 'jobs': data }); 
+    res.render('index', { 'jobs': data, 'image': svgFile }); 
 });
